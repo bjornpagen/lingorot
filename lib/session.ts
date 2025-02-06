1932759fc07ee2671c72e4f943fc755c1dfaed76
Bjorn Pagen <11238136+bjornpagen@users.noreply.github.com>
2025-02-06 13:19:04 -0600

    fatal throw

--- lib/session.ts
+++ lib/session.ts
@@ -5,13 +5,7 @@ import { unstable_headers as headers } from "expo-router/rsc/headers"
 
 export async function getSession() {
 	const reqHeaders = await headers()
-	let session: Awaited<ReturnType<typeof auth.api.getSession>> | undefined
-	try {
-		session = await auth.api.getSession({
-			headers: reqHeaders
-		})
-	} catch (error) {
-		console.log(error)
-	}
-	return session
+	return auth.api.getSession({
+		headers: reqHeaders
+	})
 }
