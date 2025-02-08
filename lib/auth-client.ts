c4f1fef8c00a889c2a90cd43e917be5b47fce666
Bjorn Pagen <11238136+bjornpagen@users.noreply.github.com>
2025-02-08 12:40:04 -0600

    auth

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -3,23 +3,8 @@
 import { createAuthClient } from "better-auth/react"
 import { expoClient } from "@better-auth/expo/client"
 import * as SecureStore from "expo-secure-store"
-import Constants from "expo-constants"
-
-const manifest = Constants.expoConfig?.extra?.expoClient
-
-const DEV_API_URL = manifest?.debuggerHost
-	? `http://${manifest.debuggerHost.split(":").shift()}:8081`
-	: "http://localhost:8081"
-
-const getBaseURL = () => {
-	if (typeof window === "undefined") {
-		return DEV_API_URL
-	}
-	return `${window.location.protocol}//${window.location.host}`
-}
 
 export const authClient = createAuthClient({
-	baseURL: getBaseURL(),
 	plugins: [
 		expoClient({
 			scheme: "myapp",
